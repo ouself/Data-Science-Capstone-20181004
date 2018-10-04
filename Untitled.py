4e747a52ba3c02f04d5f3dda83f66af8c4bacd32
ouself <43836830+ouself@users.noreply.github.com>
2018-10-04 16:12:25 -0700

    Data Science Capstone assignment 1

---
+++ Untitled.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# This notebook if for the assignment 1 of Applied Data Science Capstone course
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course!')
+
